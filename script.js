2a0ddeabe37a246768791bf42fda0f45b9254954
Madina <madinatussupova0820@gmail.com>
2022-10-19 08:48:08 +0600

    map marker

--- script.js
+++ script.js
@@ -11,11 +11,45 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-if(navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(function(position) {
-			const {latitude, longitude} = position.coords;
-			console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
-		}, function() {
-			alert("Could not get your position!");
-		});
+// Geolocation API
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude, longitude } = position.coords;
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+						const coords = [latitude, longitude];
+
+      // Map
+      const map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+							
+						map.on('click', function(mapEvent) {
+							const {lat, lng} = mapEvent.latlng;
+							L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup(L.popup({
+									maxWidth: 250,
+									minWidth: 100,
+									autoClose: false,
+									closeOnClick: false,
+									className: 'running-popup'
+								}))
+								.setPopupContent('Workout')        
+								.openPopup();
+						})
+    },
+    function () {
+      alert('Could not get your position!');
+    }
+  );
 }
